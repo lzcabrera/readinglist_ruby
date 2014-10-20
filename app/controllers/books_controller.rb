850c04909ca6d801e29c7c1c7c32830c0dfce3c0
Laura Cabrera <lzcabrera@gmail.com>
2014-10-19 19:24:42 -0700

    add preliminary search

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,7 +4,8 @@ class BooksController < ApplicationController
   # GET /books
   # GET /books.json
   def index
-    @books = Book.all
+    @books = Book.search(params[:keyword])
+
     @genres = Genre.all
   end
 
