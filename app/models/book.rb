13ec4837ecc242aaa81f21a4f905189e838ccc6a
Laura Cabrera <lzcabrera@gmail.com>
2014-10-19 20:13:54 -0700

    update search functionality

--- app/models/book.rb
+++ app/models/book.rb
@@ -6,7 +6,7 @@ class Book < ActiveRecord::Base
   #scope :finished, ->{ where('finished_on IS NOT NULL') }
   scope :finished, ->{ where.not(finished_on: nil) }
   scope :recent, ->{ where('finished_on > ?', 2.days.ago) }
-  scope :search, ->(keyword){ where(title: keyword) if keyword.present? }
+  scope :search, ->(keyword){ where('keywords LIKE ?', "%#{keyword.downcase}%") if keyword.present? }
 
   # def self.recent
   #   where('finshed_on > ?', 2.days.ago)
@@ -20,7 +20,15 @@ class Book < ActiveRecord::Base
   #   end
   # end
 
+  before_save :set_keywords
+
   def finished?
     finished_on.present?
   end
+
+  protected
+    def set_keywords
+      self.keywords = [title, author, description].map(&:downcase).join(' ')
+    end
+
 end
