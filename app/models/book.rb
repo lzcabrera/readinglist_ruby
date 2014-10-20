873c84a762550febd73afc5064dd5f83febf517f
Laura Cabrera <lzcabrera@gmail.com>
2014-10-19 17:43:23 -0700

    add bootstrap

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,8 @@
 class Book < ActiveRecord::Base
 
+  has_many :book_genres
+  has_many :genres, through: :book_genres
+
   #scope :finished, ->{ where('finished_on IS NOT NULL') }
   scope :finished, ->{ where.not(finished_on: nil) }
   scope :recent, ->{ where('finished_on > ?', 2.days.ago) }
